0b262a7e25096b97fde80bea288077c4d8e83dfe
William Reed <willmreed14@gmail.com>
2025-03-10 10:39:23 -0400

    Initialize config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,31 @@
 # Main config file for the backend
 
-# Build API using Flask 
+# Build API using Flask
 from flask import Flask
+
+# Convert SQL commands to a Python class for ease of use
+from flask_sqlalchemy import SQLAlchemy
+
+# Cross-Origin Request:
+# Send a request to this backend from a different URL.
+from flask_cors import CORS
+
+# Initialize Flask application
+app = Flask(__name__)
+
+# Wrap the app in CORS (disable CORS protection error),
+# allowing us to send cross-origin requests (backend-frontend).
+CORS(app)
+
+# Initialize database stuff
+
+# Specify location of local sqlite database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+
+# Don't track all mods we make to the database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Create an instance of the database
+# ORM: Object-Relational Mapping
+# Aka, Python class mapped to SQL database
+db = SQLAlchemy(app)
