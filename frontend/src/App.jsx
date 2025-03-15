058442cc0b6e392bbbb65ed030b31ba1a02fd20d
William Reed <willmreed14@gmail.com>
2025-03-14 21:06:34 -0400

    Create ContactForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
-import ContactList from './ContactList'
-import './App.css'
+import { useState, useEffect } from 'react';
+import ContactList from './ContactList';
+import './App.css';
 
 function App() {
   // Setup State to store and update the displaying of contacts
@@ -17,22 +17,27 @@ function App() {
       reflecting the updated state in the UI.
 
   */
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
 
   // Update the state with all contacts upon page load
   useEffect(() => {
     fetchContacts
-  }, [])
+  }, []);
 
   // Send a GET request to the .../contacts endpoint to get the contacts.
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json() // Parse the response to JSON
-    setContacts(data.contacts) // Update the useState
-    console.log(data.contacts)
-  }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json(); // Parse the response to JSON
+    setContacts(data.contacts); // Update the useState
+    console.log(data.contacts);
+  };
 
-  return <ContactList contacts={contacts}/>
+  // Render each component
+  return (
+    <>
+      <ContactList contacts={contacts} />
+    </>
+  );
 }
 
 export default App
---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,85 @@
+// Component: Contact intake form
+
+import { useState } from "react"
+
+const ContactForm = () => {
+    // State for all three variables
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    // What to do when the form is submitted
+    const onSubmit = async (e) => {
+        // Don't refresh the page when submitting the form
+        e.preventDefault()
+
+        // Setup POST request to create the contact
+
+        // Define the data we want to pass w/ the request
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+
+        // Specify the URL
+        const url = "http://127.0.0.1:5000/create_contact"
+
+        // Setup some options for the request
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json" // Specify that we're about to submit JSON
+            },
+            body: JSON.stringify(data)
+        }
+
+        const response = await fetch(url, options);
+
+        // Verify whether response is successful
+        if (response.status !== 201 && response.status !== 200) {
+            // not successful
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            // successful
+        }
+    }
+
+    // Return the form
+    return (
+        <form onSubmit={onSubmit}>
+            <div> {/* Ask user for first name */}
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                    type="text"
+                    id="firstName"
+                    value="{firstName}"
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div> {/* Ask user for last name */}
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    value="{lastName}"
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div> {/* Ask user for email */}
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    value="{email}"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            {/* On button press, call onSubmit() */}
+            <button type="submit">Create Contact</button>
+        </form>
+    );
+};
+
+export default ContactForm
